153af0fb8ccd55942e1362b3e4868ed2832ae062
philipp-do <quang.do2@sotatek.com>
2022-10-05 09:57:18 +0700

    Setup database remote

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const Career = db.career;
 const Company = db.company;
 const Job = db.job;
 db.sequelize.sync().then(() => {
-  // initial();
+  initial();
 });
 
 app.post("/uploads", upload.single("image"), function (req, res, next) {
